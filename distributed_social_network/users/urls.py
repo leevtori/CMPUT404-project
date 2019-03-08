c82a1485cb5a165c6402d7998e873df3a2242de1
shelley <stian3@ualberta.ca>
2019-03-08 18:19:50 -0500

    header menu works

--- distributed_social_network/users/urls.py
+++ distributed_social_network/users/urls.py
@@ -6,7 +6,7 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('logout/', auth_views.LogoutView.as_view(template_name="logout.html")),
+    path('logout/', auth_views.LogoutView.as_view(template_name="logout.html"), name='logout'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html')),
     path('signup/', views.SignUp.as_view()),
 ]
