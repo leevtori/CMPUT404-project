98fa749dacd60280bf5a9ad21b82d9fcfe00b27e
Kirsten Kwong <kskwong@ualberta.ca>
2019-03-10 15:29:59 -0600

    Added other required fields and changed names of fields where needed

--- distributed_social_network/api/serializers.py
+++ distributed_social_network/api/serializers.py
@@ -1,21 +1,92 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
+from posts.utils import content_type_str, visibility_str
+from posts.models import Post, Comment
+
+
 User = get_user_model()
 
-from posts.models import Post
-
 class AuthorSerializer(serializers.ModelSerializer):
+    id = serializers.SerializerMethodField()
+    firstName = serializers.CharField(source="first_name")
+    lastName = serializers.CharField(source="last_name")
+    displayName = serializers.CharField(source="display_name")
+
     class Meta:
         model = User
-        fields = ('email', 'display_name', 'github')
+        fields = (
+            "id",
+            "email",
+            "bio",
+            "host",
+            "firstName",
+            "lastName",
+            "displayName",
+            "url",
+            "github"
+        )
+
+    def get_id(self, obj):
+        return obj.get_url()
+
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    contentType = serializers.SerializerMethodField()
+    author = AuthorSerializer()
+
+    class Meta:
+        model = Comment
+        fields = (
+            "id",
+            "contentType",
+            "comment",
+            "published",
+            "author",
+        )
+
+    def get_contentType(self, obj):
+        return content_type_str[obj.content_type]
 
 
 class PostSerializer(serializers.ModelSerializer):
     count = serializers.SerializerMethodField()
+    # size = serializers.SerializerMethodField()
+    contentType = serializers.SerializerMethodField()
+    visibility = serializers.SerializerMethodField()
+    author = AuthorSerializer()
+    categories = serializers.StringRelatedField(many=True)
+    comments = CommentSerializer(many=True)
+    visibleTo = serializers.StringRelatedField(many=True, source="visible_to")
+
     class Meta:
         model = Post
-        fields = "__all__"
+        fields = (
+           "id",
+           "title",
+           "source",
+           "origin",
+           "description",
+           "contentType",
+           "content",
+           "author",
+           "categories",
+           "count",
+           "comments",
+           "published",
+           "visibility",
+           "visibleTo",
+           "unlisted"
+            )
 
     def get_count(self, obj):
-        return obj.comment_set.count()
+        return obj.comments.count()
+
+    def get_visibility(self, obj):
+        return visibility_str[obj.visibility]
+
+    def get_contentType(self, obj):
+        return content_type_str[obj.content_type]
+
+
