013fc6d62e276606f9fd5e3c8fba84a0dd11d0b0
shelley <stian3@ualberta.ca>
2019-03-11 11:49:42 -0400

    styled login & signup page

--- distributed_social_network/users/urls.py
+++ distributed_social_network/users/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path("friends/delete/", views.DeleteFriend.as_view()),
     path("friends", views.FriendList.as_view(), name='friends'),
     path('logout/', auth_views.LogoutView.as_view(template_name="logout.html"), name='logout'),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html')),
-    path('signup/', views.SignUp.as_view()),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('signup/', views.SignUp.as_view(), name='signup'),
     path('account_settings/', views.AccountSettingsView.as_view(), name='account_settings')
 ]
