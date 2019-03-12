95591fe23e1359254e95526d85386e9c39cedd82
longma1 <malong9707@hotmail.com>
2019-03-12 03:13:16 -0600

    commit in case i run reset hard like a dumbass again

--- distributed_social_network/posts/views.py
+++ distributed_social_network/posts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, HttpResponse, get_object_or_404, HttpResponseRedirect
 from django.views.generic import ListView
-from .models import Post
+from .models import Post, Comment
 from django.contrib.auth import get_user_model
 from django.views.generic.base import TemplateView
 
@@ -16,11 +16,11 @@ class ProfileView(ListView):
         context = super().get_context_data(**kwargs)
         # get user object based on username in url
         user = get_object_or_404(User, username=self.kwargs['username'])
-        #put user object in context
+        # put user object in context
         context['user'] = user
         context['friend_count'] = self.request.user.friends.count
         context['follower_count'] = self.request.user.followers.count
-        
+
         # pass context to template
         return context
 
@@ -35,14 +35,24 @@ class FeedView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['latest_posts'] = Post.objects.filter(unlisted=False)[:5]
+        # context['latest_posts'] = Post.objects.filter(unlisted=False)[:5]
+        context['latest_posts'] = Post.objects.all()[:5]
         return context
 
 
+class PostView(TemplateView):
+    template_name = 'postview.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        post = get_object_or_404(Post, id=self.kwargs['postid'])
+        context['post'] = post
+        context['post_comments'] = Comment.objects.filter(post=post)
+        return context
 
 
 def create(request):
+    print(request.POST['type'])
     # creates a post and redirects back to main page
     if request.method == "POST":
         # i dont know if i need to do this if statement yet, just gonna leave this here in case
@@ -52,25 +62,49 @@ def create(request):
                             content=request.POST['content'],
                             description=request.POST['description'],
                             content_type=request.POST['type'])
+            new_post.source = 'http://127.0.0.1:8000/posts/' + str(getattr(new_post,'id'))
             new_post.save()
-        elif request.POST['type'] == 'image/jpeg' or request.POST['type'] == 'image/png':
+
+        # turns out forms the request type as "picture/png" but the specs requires us to save as "image/png"
+        elif request.POST['type'] == 'image/jpeg' or request.POST['type'] == 'picture/png':
+            print('saving picture')
             picture_location = request.FILES['content']
             picture = picture_location.read()
-            #saves the picture
-            new_post = Post(author=request.user,
-                            title=request.POST['title'],
-                            content=picture,
-                            description=request.POST['description'],
-                            content_type=request.POST['type'],
-                            unlisted=True)
+            # saves the picture
+            if request.POST['type'] == 'image/jpeg':
+                new_post = Post(author=request.user,
+                                title=request.POST['title'],
+                                content=picture,
+                                description=request.POST['description'],
+                                content_type='image/jpeg;base64',
+                                unlisted=True)
+            else:
+                new_post = Post(author=request.user,
+                                title=request.POST['title'],
+                                content=picture,
+                                description=request.POST['description'],
+                                content_type='image/png;base64',
+                                unlisted=True)
             new_post.save()
 
-            #the next 3 lines were meant as a test, assuming that the image uploaded is a jpg
-            #this will create a copy of it in the folder of this project
+            # the next 3 lines were meant as a test, assuming that the image uploaded is a jpg
+            # this will create a copy of it in the folder of this project
             # just going to leave this here in case something breaks
 
-            #f=open('testimg.jpg','wb')
-            #f.write(picture)
-            #f.close()
+            # f=open('testimg.png','wb')
+            # f.write(picture)
+            # f.close()
 
     return HttpResponseRedirect('/')
+
+
+def create_comment(request):
+    if request.method == "POST":
+        select_post = get_object_or_404(Post, id=request.POST['post'])
+        new_comment = Comment(
+            post=select_post,
+            comment=request.POST['comment'],
+            author=request.user
+        )
+        new_comment.save()
+    return HttpResponseRedirect(select_post.source)
