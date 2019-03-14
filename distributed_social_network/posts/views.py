bcb83541ed89b06de17d11ebb93e3bc2a6b25bec
longma1 <malong9707@hotmail.com>
2019-03-14 00:53:14 -0600

    added storage of markdown and link to image, also fixed post ui

--- distributed_social_network/posts/views.py
+++ distributed_social_network/posts/views.py
@@ -5,6 +5,8 @@ from .models import Post, Comment
 from django.contrib.auth import get_user_model
 from django.views.generic.base import TemplateView
 import uuid
+import requests
+
 
 
 from django.db import connection
@@ -95,7 +97,6 @@ class PostView(PostVisbilityMixin, DetailView):
 
 
 def create(request):
-    print(request.POST['type'])
     # creates a post and redirects back to main page
     if request.method == "POST":
         # i dont know if i need to do this if statement yet, just gonna leave this here in case
@@ -109,8 +110,8 @@ def create(request):
             new_post.save()
 
         # turns out forms the request type as "picture/png" but the specs requires us to save as "image/png"
-        elif request.POST['type'] == 'image/jpeg' or request.POST['type'] == 'picture/png':
-            print('saving picture')
+        elif request.POST['type'] == 'image/jpeg' or request.POST['type'] == 'image/png':
+            print(request.POST['type'])
             picture_location = request.FILES['content']
             picture = picture_location.read()
             # saves the picture
@@ -138,6 +139,38 @@ def create(request):
             # f=open('testimg.png','wb')
             # f.write(picture)
             # f.close()
+        elif request.POST['type']=='link':
+            print('get picture')
+            response=requests.get(request.POST['content'])
+            if response.headers['Content-Type']=='image/jpeg':
+                new_post = Post(author=request.user,
+                                title=request.POST['title'],
+                                content=response.content,
+                                description=request.POST['description'],
+                                content_type='image/jpeg;base64',
+                                unlisted=True)
+                new_post.save()
+            elif response.headers['Content-Type']=='image/png':
+                new_post = Post(author=request.user,
+                                title=request.POST['title'],
+                                content=response.content,
+                                description=request.POST['description'],
+                                content_type='image/png;base64',
+                                unlisted=True)
+                new_post.save()
+                # testing purposes
+                # f=open('testimg.png','wb')
+                # f.write(response.content)
+                # f.close()
+
+        elif request.POST['type']=='text/markdown':
+            new_post = Post(author=request.user,
+                            title=request.POST['title'],
+                            content=request.POST['content'],
+                            description=request.POST['description'],
+                            content_type=request.POST['type'])
+            new_post.source = 'http://127.0.0.1:8000/posts/' + str(getattr(new_post, 'id'))
+            new_post.save()
 
     return HttpResponseRedirect('/')
 
---
+++ distributed_social_network/static/js/feed.js
@@ -0,0 +1,35 @@
+function getimageformat() {
+    var  selected_picture  = document.getElementById('createPostButton').files[0];
+    var invis_field;
+    if (selected_picture.type == 'image/jpeg') {
+            invis_field = document.getElementById("picture_type");
+            invis_field.value = 'image/jpeg';
+    }
+}
+
+function openPostInterface(post_type){
+    var selected_post;
+    selected_post = document.getElementById(post_type);
+    var all_post_divs = document.getElementsByClassName("newPost");
+    // this is dumb, but for some reason the first time this is called
+    // it returns nothing, so this is a band-aid fix
+    if ((selected_post.style.display!="none")&&(selected_post.style.display!="block")){
+        for (var i = 0; i < all_post_divs.length; i++) {
+                all_post_divs[i].style.display = "none";
+        }
+        selected_post.style.display="block";
+    }
+    else {
+        if (selected_post.style.display == "none") {
+            for (var i = 0; i < all_post_divs.length; i++) {
+                all_post_divs[i].style.display = "none";
+            }
+            selected_post.style.display = "block";
+        } else {
+            for (var i = 0; i < all_post_divs.length; i++) {
+                all_post_divs[i].style.display = "none";
+            }
+        }
+    }
+
+}
