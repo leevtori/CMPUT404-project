2baea3715763cd10def7a7973d8d7b59368a2e88
leevtori <vlee2@ualberta.ca>
2019-03-26 22:41:04 -0400

    making admin more usable"

--- distributed_social_network/posts/admin.py
+++ distributed_social_network/posts/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
 from .models import Post, Comment
 
+
+
+class CustomPostAdmin(admin.ModelAdmin):
+    model = Post
+    list_display = ['title', 'author']
+
+class CustomCommentAdmin(admin.ModelAdmin):
+    model = Comment
+    list_display = ['comment', 'author', 'get_post_title']
+
+    def get_post_title(self, obj):
+        return obj.post.title
+    get_post_title.short_description = "Post Title"
+
+
 # Register your models here.
-admin.site.register(Post)
-admin.site.register(Comment)
+admin.site.register(Post, CustomPostAdmin)
+admin.site.register(Comment, CustomCommentAdmin)
