a9c781890941d191d36b949955122798f10b4d12
Kirsten Kwong <kskwong@ualberta.ca>
2019-03-28 15:52:32 -0600

    Added page size to paginator

--- distributed_social_network/api/paginators.py
+++ distributed_social_network/api/paginators.py
@@ -6,6 +6,9 @@ class CustomPaginator(pagination.PageNumberPagination):
     def get_paginated_response(self, data, **kwargs):
         response_data = {}
         query = kwargs.get("query", None)
+        response_data["count"] = self.page.paginator.count
+        response_data["size"] = self.page_size
+
         if query:
             response_data["query"] = query
         if self.get_next_link():
@@ -13,8 +16,6 @@ class CustomPaginator(pagination.PageNumberPagination):
 
         if self.get_previous_link():
             response_data["previous"] = self.get_previous_link()
-
-        response_data["count"] = self.page.paginator.count
 
         key = kwargs.get("model", "data")
         response_data[key] = data
